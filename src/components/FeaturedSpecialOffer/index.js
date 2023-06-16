2ba08953cadad1f5988279f12e5f6a7b68bd5e53
Gabriela Queiroz <gabrielapg.queiroz@gmail.com>
2023-06-16 10:55:31 -0300

    ajustes finais special offer

--- src/components/FeaturedSpecialOffer/index.js
+++ src/components/FeaturedSpecialOffer/index.js
@@ -15,7 +15,7 @@ export default function FeaturedSpecialOffer() {
     const Card = (props) => {
         return (
             <>
-                <Grid item xs={5}>
+                <Grid item xs={6}>
                     <div className="card">
                         <img src={props.image} alt="" />
                     </div>
