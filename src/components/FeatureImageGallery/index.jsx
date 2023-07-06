e7fe82169c3bd3e0cee1363ad92888949dc23059
Gabriela Queiroz <gabrielapg.queiroz@gmail.com>
2023-07-06 12:33:07 -0300

    Refatoração: Componentização da seção CardsMain utilizando o useState

--- src/components/FeatureImageGallery/index.jsx
+++ src/components/FeatureImageGallery/index.jsx
@@ -20,7 +20,17 @@ export default function FeatureImageGallery() {
             .then(data => setItems(data)); // SETA O USESTATE E ATUALIZA ITEMS COM AS INFORMAÇÕESDO ARRAY
     }, []);
 
-
+      const CardsMain = (props) => {
+      return(
+      <>
+      <Grid item xs={6}>
+        <div className='divimagecarrocel'>
+          <button><img src={arrowleft} alt="" className='arrows' /></button>
+            <img className='tenismain' src={props.image} alt="" />
+          <button><img src={arrowright} alt="" className='arrows'/></button>
+        </div>
+      </Grid>
+      
       <Grid item xs={6}>
         <div className="componentright">
           <h1>{props.title}</h1>
